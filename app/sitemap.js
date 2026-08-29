36ccb41e3413c50e160bf1105fdd707eb0159b31
16flames <nandapauji08@gmail.com>
2026-08-29 14:54:06 +0700

    add sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,18 @@
+export default function sitemap() {
+  const baseUrl = "https://www.16flames.store";
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+  ];
+}
